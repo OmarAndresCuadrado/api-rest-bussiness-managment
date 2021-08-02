51c9a7ef0512875d679a2026b6f9e6de8b48951f
OmarAndresCuadrado <omarcuadradodev@gmail.com>
2021-08-02 11:20:07 -0600

    commit test

--- src/test/java/com/backend/api/tests/integration/ProductIntegrationTest.java
+++ src/test/java/com/backend/api/tests/integration/ProductIntegrationTest.java
@@ -1,12 +1,12 @@
-package com.backend.api.tests.integration;
-
-/**
- * @author MrRobot
- *
- */
-
-public class ProductIntegrationTest {
-
-
-
-}
+//package com.backend.api.tests.integration;
+//
+///**
+// * @author MrRobot
+// *
+// */
+//
+//public class ProductIntegrationTest {
+//
+//
+//
+//}
